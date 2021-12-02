2470fb6002bfde8b1e49af4c14ccf1ffab2dfb99
MarwenHihi <hihi.marwen33@gmail.com>
2021-12-02 15:16:52 +0100

    make list

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ class taquin :
                 list_random.append(r)
 
         for i in range(0,n):
-                self.matrice[i]=list_random[-3:]
-                list_random.pop()
-                list_random.pop()
-                list_random.pop()
+                self.matrice[i]=list_random[-n:]
+                for j in range(0,n):
+                    list_random.pop()
+               
         return(self.matrice)
                             
     def trans(matrice,i,j,token): #win bch yemchi
-        if token!="none":
+        if token!="null":
             if token=="up":
                 matrice[i-1][j]=matrice[i][j]
             
@@ -42,24 +42,40 @@ class taquin :
             matrice[i][j]=0
 
     def verif_mvt(self,matrice,i,j): #verif anehi l case l fergha
-        if matrice[i-1][j]==0 and i>0:
+        if i>0 and matrice[i-1][j]==0:
             return("up")
-        elif matrice[i+1][j]==0 and i+1<self.n:
+        elif i+1<self.n and matrice[i+1][j]==0 :
             return("down")
-        elif matrice[i][j+1]==0 and j+1<self.n:
+        elif j+1<self.n and matrice[i][j+1]==0 :
             return("right")
-        elif matrice[i][j-1]==0and j>0:
+        elif j>0 and matrice[i][j-1]==0:
             return("left")
         else :
-            return("none")
+            return("null")
          
     def verif_final(self):
 
-        if self==self.etat_final:
+        if self.matrice==self.matrice_final:
             return True
         else : return False
         
     
+    def make_list(self):
+        i=j=0
+        while i in range(0,self.n):
+            while j in range(0,self.n):
+                token=self.verif_mvt(self.matrice,i,j)
+                if token!="null":
+                    self.list.append(self.trans(i,j,token))
+        
+    def arbre(self):
+        i=j=0
+        while self.verif_final()==False:
+            
+                    return
+                
+                
+                
 
 size=int(input("saisir la taille : "))
 mat=taquin(size)
