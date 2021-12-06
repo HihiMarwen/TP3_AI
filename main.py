e176f80a8cab6b4658e6dd0699aea5a6886ab0b1
selma ayachi <salma.ayachi@insat.u-carthage.tngit config --global user.name selma>
2021-12-06 17:07:46 +0100

    2nd commit

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ class taquin :
         self.list=[]
         self.matrice=[[]]
         self.matrice_final=[[]]
+        self.cout=0
+        self.niv=0
+
+    def mat_final(self,n):
+        self.matrice_final=copy.deepcopy(self.matrice)
+
+        #construire une liste contenant des entiers random et unique
+        list_random = []
+        while len(list_random) < n*n:
+        #This checks to see if there are duplicate numbers
+            r = np.random.randint(0,n*n)
+            if r not in list_random :
+                list_random.append(r)
+        list_random.sort()
+
+        for i in range(0,n):
+            self.matrice_final[n-i-1]=list_random[-n:]
+            for j in range(0,n):
+                list_random.pop()
+        return(self.matrice_final)
         
     def const_random_mat(self,n):
         self.matrice = np.random.randint(n*n, size=(n,n))
@@ -19,7 +39,7 @@ class taquin :
                 list_random.append(r)
 
         for i in range(0,n):
-                self.matrice[i]=list_random[-n:]
+                self.matrice[n-i-1]=list_random[-n:]
                 for j in range(0,n):
                     list_random.pop()
                
@@ -56,35 +76,54 @@ class taquin :
         else :
             return("null")
          
-    def verif_final(self):
-
-        if self.matrice==self.matrice_final:
-            return True
-        else : return False
+    def verif_final(self,matrice):
+        for i in range(0,self.n):
+            for j in range(0,self.n):
+                
+                if matrice[i][j]!=self.matrice_final[i][j]:
+                    return False
+        return True
         
     
-    def make_list(self):
+    def make_list(self,matrice):
         
         for i in range(0,self.n):
             for j in range(0,self.n):
-                print(i,j)
-                token="a"
-                token=self.verif_mvt(self.matrice,i,j)
-                print(token)
+                token=matrice.verif_mvt(self.matrice,i,j)
+                
                 if token!="null":
-                    self.list.append(self.trans(i,j,token))
+                    matrice.list.append(self.trans(i,j,token))
+        return(matrice)
         
-    def arbre(self):
-        i=j=0
-        while self.verif_final()==False:
-            
-                    return
+    def arbre(self,matrice):
+        if self.verif_final(matrice.matrice)==False and self.cout<100:
+            matrice=matrice.make_list(matrice)
+            for i in range(0,len(matrice.list)):
+                self.cout+=1
+                matrice.niv+=1
+                verif=self.arbre(matrice.list[i])
+               
+                return True
                 
-                
+        else : return(True)        
                 
 
 size=int(input("saisir la taille : "))
 mat=taquin(size)
 mat.const_random_mat(size)
-print(mat.matrice)
+print("Taquin Initial: \n ",mat.matrice)
+mat.mat_final(size)
+print("Taquin final: \n ",mat.matrice_final)
+
+mat.arbre(mat)
+if mat.cout >= 100:
+    print("solution pas trouvée")
+else:
+    print("Solution Trouvée\n Cout = ",mat.cout)
+
+'''mat.make_list()
+print("Les cas possibles")
+for i in range(0,len(mat.list)):
+    print(mat.list[i])
+    print("********")'''
 
